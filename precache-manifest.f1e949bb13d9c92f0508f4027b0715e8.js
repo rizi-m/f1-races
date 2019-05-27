bc9b8ba78f46c260126a9bdaacd68bcca3d8ada3
Rizwan Mahmood <rizwanmahmood255@gmail.com>
2019-05-27 17:46:26 +0100

    Updates

---
+++ precache-manifest.f1e949bb13d9c92f0508f4027b0715e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f6febc4fae4c628c858c570b4ec0c1",
+    "url": "/f1-races/index.html"
+  },
+  {
+    "revision": "7a0548a2d3df2c5320c2",
+    "url": "/f1-races/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/f1-races/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "7a0548a2d3df2c5320c2",
+    "url": "/f1-races/static/js/main.f77af3fa.chunk.js"
+  },
+  {
+    "revision": "bd231a494cfd57774652",
+    "url": "/f1-races/static/js/runtime~main.fd45dc31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/f1-races/static/media/logo.5d5d9eef.svg"
+  }
+]);
